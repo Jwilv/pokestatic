4bf0f9e15ae5d69b46179065725890f85fd4fa8c
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-09-14 14:38:35 -0300

    barrel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import MainLayout from "@/components/layouts/MainLayout";
+import { MainLayout } from "@/components/layouts";
 import { Button, Card } from "@nextui-org/react";
 import { useTheme } from "next-themes";
 
