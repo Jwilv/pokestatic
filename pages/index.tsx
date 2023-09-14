a352f4158fd2382ffba6e5db13487317f20c411d
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-09-14 11:07:20 -0300

    tyailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
+import { Button, Card } from "@nextui-org/react";
 
 export default function HomePage() {
   return (
     <>
-      <h1>Home</h1>
+      {/* <Card>
+        <Button size='sm' color='primary'>Home aaaaaaaaaaa</Button>
+        <Button size='lg' color='secondary'>Home bbbbbbbbbbb</Button>
+      </Card> */}
+
+      <h1 className="underline">hola</h1>
     </>
   )
 }
