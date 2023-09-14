dd57b0f8710ab6225747ebd82470d626ea856387
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-09-14 14:12:07 -0300

    change theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import { Button, Card } from "@nextui-org/react";
+import { useTheme } from "next-themes";
 
 export default function HomePage() {
+
+  const { setTheme } = useTheme();
   return (
     <>      
-        <Button size='sm' color='primary'>Home aaaaaaaaaaa</Button>
-        <Button size='lg' color='secondary'>Home bbbbbbbbbbb</Button>
+        <Button 
+        size='sm' 
+        color='primary'
+        onClick={() => setTheme('light')}
+        >Home aaaaaaaaaaa</Button>
+        <Button 
+        size='lg' 
+        color='secondary'
+        onClick={() => setTheme('dark')}
+        >Home bbbbbbbbbbb</Button>
     </>
   )
 }
