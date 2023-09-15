78a0af4011bef15e968f9913e7b817e66e48b37a
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-09-15 15:45:34 -0300

    navbar

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,18 +1,44 @@
+import { Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
 import Head from "next/head"
 
 interface Props {
-    children: React.ReactNode
+    children: React.ReactNode;
+    title?: string;
 }
 
-const MainLayout = ({ children }: Props) => {
+const MainLayout = ({ children, title }: Props) => {
     return (
         <>
             <Head>
-                <title>poke app</title>
+                <title>{title || 'poke app'}</title>
                 <meta name="description" content="app for poke" />
                 <meta name='author' content="app for poke" />
             </Head>
+
             <main>
+            <Navbar shouldHideOnScroll maxWidth="full">
+                <NavbarContent >
+                    <NavbarBrand>
+                        <h2>P</h2>
+                        <h3>okemon</h3>
+                    </NavbarBrand>
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Features
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Features
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Features
+                        </Link>
+                    </NavbarItem>
+                </NavbarContent>
+            </Navbar>
                 {children}
             </main>
         </>
