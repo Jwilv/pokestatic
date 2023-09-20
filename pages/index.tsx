3a8dc6de104c397d4dcd9530673e92bc898736e9
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-09-20 14:37:26 -0300

    map pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,50 @@
+import { pokeApi } from "@/api";
 import { MainLayout } from "@/components/layouts";
-import { Button, Card } from "@nextui-org/react";
-import { GetStaticProps, NextPage } from "next";
-import { useTheme } from "next-themes";
+import { PokemonListResponse, SmallPokemon } from "@/interfaces";
+import { Card, CardBody, CardFooter } from "@nextui-org/react";
+import { GetStaticProps } from "next";
+import Image from "next/image";
 
 interface HomePageProps {
   getLayout: (page: JSX.Element) => JSX.Element
 }
 
-const HomePage: HomePageProps = (props: {}) => {
+interface Props {
+  pokemons: SmallPokemon[]
+}
 
-  console.log(props)
+const HomePage: HomePageProps = ({ pokemons }: Props) => {
 
-  const { setTheme } = useTheme();
   return (
-    <>
-      <Button
-        size='sm'
-        color='primary'
-        onClick={() => setTheme('light')}
-      >Home aaaaaaaaaaa</Button>
-      <Button
-        size='lg'
-        color='secondary'
-        onClick={() => setTheme('dark')}
-      >Home bbbbbbbbbbb</Button>
-    </>
+    <div 
+    className="poke-container"
+    >
+      
+      {
+        pokemons.map(({ id, name, img }) => (
+          <Card
+            key={id}
+            isPressable
+            className="poke-card"
+          >
+            <CardBody
+            className="poke-body"
+            >
+              <Image
+                width={160}
+                height={160}
+                alt={name}
+                className="object-cover h-[140px]"
+                src={img}
+              />
+            </CardBody>
+            <CardFooter className="poke-footer">
+              <b>#{id} {name}</b>
+            </CardFooter>
+          </Card>
+        ))
+      }
+    </div>
   )
 }
 
@@ -44,11 +64,17 @@ interface StaticProps {
 
 export const getStaticProps: GetStaticProps = async (ctx): Promise<StaticProps> => {
 
-  console.log('hola mundo')
-
+  const { data } = await pokeApi.get<PokemonListResponse>('pokemon?limit=151');
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, i) => (
+    {
+      ...pokemon,
+      id: i + 1,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i + 1}.svg`
+    }
+  ))
   return {
     props: {
-      data: null
+      pokemons
     }
   }
 }
