120e2b4fe6ba8dd59ee0ad6650d9480f000b2aa8
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-09-21 19:21:19 -0300

    getStaticPaths and  getStaticProps

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,15 +1,50 @@
+import { pokeApi } from "@/api";
+import { Pokemon } from "@/interfaces";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router"
 
+interface Props {
+  pokemon: Pokemon
+}
 
- const PokemonPage = () => {
+const PokemonPage = ({ pokemon }: Props) => {
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const { id } = router.query
+  const { id } = router.query
   return (
     <div>
+      <h1>{id}</h1>
     </div>
   )
 }
 
 export default PokemonPage
+
+export const getStaticPaths: GetStaticPaths = async () => {
+
+  const pokemonsPaths: string[] = [...Array(151)].map((_value, index) => (`${index + 1}`))
+
+  return {
+    paths: pokemonsPaths.map((value) => ({
+      params: {
+        id: value
+      }
+    })),
+    fallback: false
+  }
+
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+
+  const { id } = params as { id: string }
+
+  const pokemon = await pokeApi.get<Pokemon>(`pokemon/${id}`)
+
+  return {
+    props: {
+      pokemon
+    }
+  }
+}
